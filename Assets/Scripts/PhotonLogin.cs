de993c47f5133f18c2dcd87db687cba02c523ebe
Danil <danilstasilevich@yandex.ru>
2022-01-22 19:20:00 +0300

    Added connection buttons

--- Assets/Scripts/PhotonLogin.cs
+++ Assets/Scripts/PhotonLogin.cs
@@ -1,33 +1,72 @@
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class PhotonLogin : MonoBehaviourPunCallbacks
 {
+    [SerializeField] private ConnectionButtonWidget _connectButton;
+    [SerializeField] private ConnectionButtonWidget _disconnectButton;
+
     private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
+
+        _connectButton.Refresh(ConnectionState.Default, "Connect to Photon button");
+        _disconnectButton.Refresh(ConnectionState.Default, "Disconnect from Photon button");
+
+        _connectButton.button.onClick.AddListener(OnConnectButtonClick);
+        _disconnectButton.button.onClick.AddListener(OnDisconnectButtonClick);
     }
 
-    private void Start()
+    private void OnDestroy()
     {
-        Connect();
+        _connectButton.button.onClick.RemoveListener(OnConnectButtonClick);
+        _disconnectButton.button.onClick.RemoveListener(OnDisconnectButtonClick);
     }
 
-    public void Connect()
+    void OnConnectButtonClick()
     {
+        _disconnectButton.Refresh(ConnectionState.Default, "Disconnect from Photon button");
+
         if (PhotonNetwork.IsConnected)
         {
             PhotonNetwork.JoinRandomRoom();
         }
         else
         {
+            _connectButton.Refresh(ConnectionState.Waiting, "Waiting for connection to Photon");
             PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    void OnDisconnectButtonClick()
+    {
+        _connectButton.Refresh(ConnectionState.Default, "Connect Photon button");
+
+        if (PhotonNetwork.IsConnected)
+        {
+            _disconnectButton.Refresh(ConnectionState.Waiting, "Waiting for disconnection from Photon");
+            PhotonNetwork.Disconnect();
         }
     }
 
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
-        Debug.Log("Photon Success");
+
+        string photonSuccessText = "Successfuly connected to Photon";
+        
+        Debug.Log(photonSuccessText);
+        _connectButton.Refresh(ConnectionState.Success, photonSuccessText);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+
+        string photonSuccessText = "Successfuly disconnected from Photon";
+        
+        Debug.Log(photonSuccessText);
+        _disconnectButton.Refresh(ConnectionState.Success, photonSuccessText);
     }
 }
