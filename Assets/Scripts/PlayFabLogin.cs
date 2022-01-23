569431b4658da958d370a479e2125b9f0a680720
Dmitriy Starokozhev <p1cachu@yandex.ru>
2022-01-23 20:26:52 +0300

    login to PlayFab and getting account info

--- Assets/Scripts/PlayFabLogin.cs
+++ Assets/Scripts/PlayFabLogin.cs
@@ -1,9 +1,64 @@
 using PlayFab;
 using PlayFab.ClientModels;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayFabLogin : MonoBehaviour
 {
+    private string _username;
+    private string _mail;
+    private string _pass;
+
+    private const string AuthKey = "player-unique-id";
+    public void UpdateUsername(string username)
+    {
+        _username = username;
+    }
+
+    public void UpdateEmail(string mail)
+    {
+        _mail = mail;
+    }
+
+    public void UpdatePass(string pass)
+    {
+        _pass = pass;
+    }
+
+    public void CreateAccount()
+    {
+        PlayFabClientAPI.RegisterPlayFabUser(new RegisterPlayFabUserRequest
+        {
+            Username = _username,
+            Email = _mail,
+            Password = _pass,
+            RequireBothUsernameAndEmail = true
+        }, result =>
+        {
+            Debug.Log("Success");
+        }, errorCallback => {
+            Debug.LogError($"Error: {errorCallback}");
+        });
+    }
+
+    public void Login()
+    {
+        
+        PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest
+        {
+            Username = _username,
+            Password = _pass
+        }, result =>
+        {
+            
+            Debug.Log($"Success: {_username}");
+        }, errorCallback => {
+            Debug.LogError($"Error: {errorCallback}");
+        });
+    }
+
     private void Start()
     {
         if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
@@ -12,13 +67,16 @@ public class PlayFabLogin : MonoBehaviour
             Debug.Log("Title ID was installed");
         }
 
-        var request = new LoginWithCustomIDRequest {CustomId = "lesson3", CreateAccount = true};
-        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
-    }
-
-    private void OnLoginSuccess(LoginResult result)
-    {
-        Debug.Log("PlayFab Success");
+        var needCreation = !PlayerPrefs.HasKey(AuthKey);
+        Debug.Log($"needCreation = {needCreation}");
+        var id = PlayerPrefs.GetString(AuthKey, Guid.NewGuid().ToString());
+        Debug.Log($"id = {id}");
+        var request = new LoginWithCustomIDRequest {CustomId = id, CreateAccount = needCreation};
+        PlayFabClientAPI.LoginWithCustomID(request, reuslt =>
+        {
+            PlayerPrefs.SetString(AuthKey, id);
+            SceneManager.LoadScene("MainProfile");
+        }, OnLoginFailure);
     }
 
     private void OnLoginFailure(PlayFabError error)
