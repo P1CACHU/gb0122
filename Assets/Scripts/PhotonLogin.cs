d0166ed7da629f69b53412effe451c8d97d82b70
FeshchenkoIvan <vaneskrutoy@mail.ru>
2022-01-28 20:41:16 +0300

    lesson4

--- Assets/Scripts/PhotonLogin.cs
+++ Assets/Scripts/PhotonLogin.cs
@@ -1,23 +1,31 @@
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PhotonLogin : MonoBehaviourPunCallbacks
 {
+    public Button button;
     private void Awake()
     {
+        //как только MasterClient вызывает загрузку следующей сцены методом
+        //PhotonNetwork.LoadLevel(), все подключенные к нему игроки
+        //автоматически делают то же самое
         PhotonNetwork.AutomaticallySyncScene = true;
     }
 
     private void Start()
     {
-        Connect();
+        //Connect();
     }
 
+    
     public void Connect()
     {
         if (PhotonNetwork.IsConnected)
         {
-            PhotonNetwork.JoinRandomRoom();
+            //PhotonNetwork.JoinRandomRoom();
+            PhotonNetwork.Disconnect();
         }
         else
         {
@@ -25,9 +33,29 @@ public class PhotonLogin : MonoBehaviourPunCallbacks
         }
     }
 
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        if (cause== DisconnectCause.DisconnectByClientLogic)
+        {
+            button.transform.GetChild(0).GetComponent<Text>().text = "Log In";
+            button.GetComponent<Image>().color = Color.white;
+            Debug.Log(cause);
+            Debug.Log("Photon Disconnect");
+        }
+        else
+        {
+            Debug.Log("Error: "+cause);
+            button.GetComponent<Image>().color = Color.red;
+        }
+
+    }
+
     public override void OnConnectedToMaster()
     {
+        button.transform.GetChild(0).GetComponent<Text>().text = "Disconnect";
+        button.GetComponent<Image>().color = Color.green;
         base.OnConnectedToMaster();
         Debug.Log("Photon Success");
     }
+
 }
