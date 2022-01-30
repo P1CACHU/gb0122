a4752bc7745bd1c2ac923d2ebff1d6fb60ae9446
Maks <56753588+BloodyBorsch@users.noreply.github.com>
2022-01-30 17:39:03 +0300

    Update PhotonLogin.cs

--- Assets/Scripts/ExampleScripts/MenuManagement/PhotonLogin.cs
+++ Assets/Scripts/ExampleScripts/MenuManagement/PhotonLogin.cs
@@ -1,5 +1,7 @@
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 
@@ -11,7 +13,10 @@ namespace ExampleGB
         public event OnMessageRecieve OnRecieveMSG;
 
         private const string CONNECTION = "Photon connection Success";
-        private const string DISCONNECTION = "Photon disconnection";        
+        private const string DISCONNECTION = "Photon disconnection";
+
+        [SerializeField] private TMP_InputField _roomName;
+        [SerializeField] private TMP_InputField _maxPlayers;
 
         private void Awake()
         {
@@ -50,6 +55,25 @@ namespace ExampleGB
             base.OnDisconnected(cause);
             OnRecieveMSG?.Invoke(DISCONNECTION);
             Debug.Log(DISCONNECTION);
+        }
+
+        public override void OnRoomListUpdate(List<RoomInfo> roomList)
+        {
+            Debug.Log("OnRoomListUpdate Success");
+        }
+
+        public void OnCreateRoomButtonClicked()
+        {
+            string roomName = _roomName.text;
+            roomName = (roomName.Equals(string.Empty)) ? "Room " + Random.Range(1000, 10000) : roomName;
+
+            byte maxPlayers;
+            byte.TryParse(_maxPlayers.text, out maxPlayers);
+            maxPlayers = (byte)Mathf.Clamp(maxPlayers, 2, 8);
+
+            RoomOptions options = new RoomOptions { MaxPlayers = maxPlayers, PlayerTtl = 10000 };
+
+            PhotonNetwork.CreateRoom(roomName, options);
         }
     }
 }
